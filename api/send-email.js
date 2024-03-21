2983e522ddb7589f27a37d0472fa6c29e6c33523
StefaniaPaduraru <stefania.paduraru@student.tuiasi.ro>
2024-03-21 21:12:11 +0200

    Update AJAX URL to /api/send-email

---
+++ api/send-email.js
@@ -0,0 +1,32 @@
+// /api/send-email.js
+require('dotenv').config();
+const nodemailer = require('nodemailer');
+
+module.exports = (req, res) => {
+    const { name, email, message } = req.body;
+
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS
+        }
+    });
+
+    var mailOptions = {
+        from: process.env.EMAIL_USER,
+        to: process.env.EMAIL_USER,
+        subject: 'New Contact Form Submission',
+        text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+            res.status(500).send('Error sending email');
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.send('Email sent successfully');
+        }
+    });
+};
